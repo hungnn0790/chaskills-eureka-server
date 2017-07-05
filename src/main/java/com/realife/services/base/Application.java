e5b3a89ee6ea2ce7ed2979980b6c65eb5073137a
hungnn0790 <hungnn0790@gmail.com>
2017-07-06 04:11:06 +0700

    Refactor packages name

--- src/main/java/com/realife/services/base/Application.java
+++ src/main/java/com/realife/services/base/Application.java
@@ -1,4 +1,4 @@
-package com.realife.services.realife_eureka_server;
+package com.realife.services.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +10,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// Tell Boot to look for registration-server.yml
-//		System.setProperty("spring.config.name", "registration-server");
+		// System.setProperty("spring.config.name", "registration-server");
 		SpringApplication.run(Application.class, args);
 	}
 }
